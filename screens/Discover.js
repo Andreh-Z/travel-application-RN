b60ea8892e6d482538d2c98dd0ac79bfe61813c1
Andreh-Z <witecan931@gmail.com>
2023-02-03 20:48:56 -0300

    google_places

--- screens/Discover.js
+++ screens/Discover.js
@@ -1,6 +1,8 @@
-import { View, Text } from "react-native";
+import { View, Text, SafeAreaView, Image, ScrollView } from "react-native";
 import React, { useLayoutEffect } from "react";
 import { useNavigation } from "@react-navigation/native";
+import { Avatar } from "../assets";
+import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
 
 export default function Discover() {
   const navigation = useNavigation();
@@ -11,8 +13,37 @@ export default function Discover() {
   }, []);
 
   return (
-    <View>
-      <Text>Discover</Text>
-    </View>
+    <SafeAreaView className="flex-1 bg-white relative">
+      <View className="flex-row items-center justify-between px-8">
+        <View>
+          <Text className="text-[40px] text-[#0B646B] font-bold">Discover</Text>
+          <Text className="text-[#527283] text-[36px]">the beauty today</Text>
+        </View>
+        <View className="w-12 h-12 bg-gray-400 rounded-md justify-center items-center shadow-lg">
+          <Image
+            source={Avatar}
+            className="w-full h-full rounded-md object-cover"
+          />
+        </View>
+      </View>
+      {/* View for input search */}
+      <View className="flex-row items-center bg-white mx-4 rounded-xl shadow-xl shadow-slate-700 py-1 px-4 mt-4">
+        <GooglePlacesAutocomplete
+          placeholder="Search"
+          onPress={(data, details = null) => {
+            // 'details' is provided when fetchDetails = true
+            console.log(data, details);
+          }}
+          query={{
+            key: "YOUR API KEY",
+            language: "en",
+          }}
+        />
+      </View>
+      {/* Menus containers */}
+      <ScrollView>
+        <View className="flex-row items-center justify-center px-8 mt-8"></View>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
