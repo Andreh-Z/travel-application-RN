4f0325ff9bd07f38aa3cc05c9a59534aa72bb0aa
Andreh-Z <witecan931@gmail.com>
2023-02-03 14:47:03 -0300

    assets

--- App.js
+++ App.js
@@ -1,8 +1,14 @@
 import * as React from "react";
 import { NavigationContainer } from "@react-navigation/native";
-
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import HomeScreen from "./screens/HomeScreen";
+const Stack = createNativeStackNavigator();
 export default function App() {
   return (
-    <NavigationContainer>{/* Rest of your app code */}</NavigationContainer>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
